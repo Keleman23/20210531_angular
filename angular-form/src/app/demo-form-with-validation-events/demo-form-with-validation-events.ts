b8665ebef108256dd184616a9cf4f649f6df6106
Kelemen <istvan@kelemen.hu>
2021-06-01 13:54:22 +0200

    events

--- angular-form/src/app/demo-form-with-validation-events/demo-form-with-validation-events.ts
+++ angular-form/src/app/demo-form-with-validation-events/demo-form-with-validation-events.ts
@@ -8,12 +8,16 @@ import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/fo
 export class DemoFormWithValidationEventsComponent implements OnInit {
   myForm: FormGroup;
   sku: AbstractControl;
+
   constructor(fb: FormBuilder) {
     this.myForm = fb.group({
       'sku': ['', Validators.required]
     });
 
     this.sku = this.myForm.controls['sku'];
+
+    this.sku.valueChanges.subscribe((value: string) => {console.log('sku changed:',value);});
+    this.myForm.valueChanges.subscribe((formValue: any) => {console.log('form changed:',formValue);});
   }
 
   ngOnInit(): void {
